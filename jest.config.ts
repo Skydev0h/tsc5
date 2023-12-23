cf4c777d6d3bac407e97e884fad9147e22863797
Skydev0h <alexhacker64@gmail.com>
2023-12-24 01:28:50 +0200

    safety

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+    testPathIgnorePatterns: ['/node_modules/', '/dist/', '/tests/off/'],
 };
 
 export default config;
